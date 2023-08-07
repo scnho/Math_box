472968d6f973b2ce0a522a5f84738c5c997abcd2
scnho <140799524+scnho@users.noreply.github.com>
2023-08-07 17:38:53 +0900

    Add say_hello function

--- calculator.py
+++ calculator.py
@@ -1,16 +1,16 @@
 # 기본 개산기
 
-def add(a, b);
+def add(a, b):
     return a+b
 
-def subtract(a, b);
+def subtract(a, b):
     return a-b
 
-
-
-git add
-def multiply(a, b);
+def multiply(a, b):
     return a*b    
 
 def divide_new(a, b):  
     return a/b 
+    
+def say_hello:  
+    print("hello") 
